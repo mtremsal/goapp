f854af2218a613da34ef55d71e852f4f165a3dd2
mtremsal <marc.tremsal@gmail.com>
2018-01-04 18:17:39 -0500

    Instrument app with dd-trace-go for mux

--- app.go
+++ app.go
@@ -5,17 +5,29 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/DataDog/dd-trace-go/tracer"
+	"github.com/DataDog/dd-trace-go/tracer/contrib/gorilla/muxtrace"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", Hello)
+	// muxTracer := muxtrace.NewMuxTracer("goapp", tracer.DefaultTracer)
+	// muxTransport := tracer.NewTransport("localhost", "8126") // Default
+	// where docker.for.mac.localhost is the host's IP
+	muxTransport := tracer.NewTransport("docker.for.mac.localhost", "8126")
+	muxTracer := muxtrace.NewMuxTracer("goapp", tracer.NewTracerTransport(muxTransport))
+	muxTracer.HandleFunc(r, "/", hello)
+	r.HandleFunc("/", hello)
 	http.Handle("/", r)
 	fmt.Println("Starting up on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func Hello(w http.ResponseWriter, req *http.Request) {
+// HandleFunc for '/'
+func hello(w http.ResponseWriter, req *http.Request) {
+	span := tracer.SpanFromContextDefault(req.Context())
+	fmt.Printf("tracing service:%s resource:%s", span.Service, span.Resource)
 	fmt.Fprintln(w, "Hello world!")
+	// w.Write([]byte("hello world"))
 }
