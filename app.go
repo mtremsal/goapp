509b9fe1c3dd6c68b3a3be5b5c423148534533a3
mtremsal <marc.tremsal@gmail.com>
2018-01-04 19:32:21 -0500

    Implement nested spans of random duration

--- app.go
+++ app.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/DataDog/dd-trace-go/tracer"
 	"github.com/DataDog/dd-trace-go/tracer/contrib/gorilla/muxtrace"
@@ -18,7 +20,7 @@ func main() {
 	muxTransport := tracer.NewTransport("docker.for.mac.localhost", "8126")
 	muxTracer := muxtrace.NewMuxTracer("goapp", tracer.NewTracerTransport(muxTransport))
 	muxTracer.HandleFunc(r, "/", hello)
-	r.HandleFunc("/", hello)
+	// r.HandleFunc("/", hello) // not required since we use muxTracer
 	http.Handle("/", r)
 	fmt.Println("Starting up on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -27,7 +29,20 @@ func main() {
 // HandleFunc for '/'
 func hello(w http.ResponseWriter, req *http.Request) {
 	span := tracer.SpanFromContextDefault(req.Context())
-	fmt.Printf("tracing service:%s resource:%s", span.Service, span.Resource)
+	fmt.Printf("tracing service:%s resource:%s\n", span.Service, span.Resource)
 	fmt.Fprintln(w, "Hello world!")
-	// w.Write([]byte("hello world"))
+	i := rand.Intn(10)
+	nestedHelloWithRandomSleep(w, req, i)
+}
+
+func nestedHelloWithRandomSleep(w http.ResponseWriter, req *http.Request, level int) int {
+	if level <= 0 {
+		return 0
+	}
+	span := tracer.NewChildSpanFromContext("mux.request.nested-hello", req.Context())
+	r := rand.Intn(10) * 10
+	time.Sleep(time.Duration(r) * time.Microsecond)
+	fmt.Fprintln(w, "Nested Hello at level:", level, ". Slept for:", r)
+	defer span.Finish()
+	return nestedHelloWithRandomSleep(w, req, level-1)
 }
